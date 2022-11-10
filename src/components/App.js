a4b26a3ed65023c67912233c5b4f9094f53cd0e1
이고운 <kowoon@igoun-ui-MacBookAir.local>
2022-11-10 17:41:17 +0900

    Add: todo 기본 세팅

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Detail from "../pages/Detail";
+import Home from "../pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const Home = () => {
+  const [text, setText] = useState("");
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText("");
+  };
+
+  return (
+    <>
+      <h1>To do</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={handleChange} />
+        <button>Add</button>
+      </form>
+      <ul></ul>
+    </>
+  );
+};
+export default Home;
